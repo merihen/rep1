c8bc2bc5919d9b101ce78d4e46dbd019349901cc
root <root@merihenManager.33dpyidlgvxuvkuqi2bysxqwkg.bx.internal.cloudapp.net>
2020-01-31 10:13:24 +0000

    ficher ajouté

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload ye merihen(tawa 9a3ed ijib mel volume+mara okhra*2) .
         </p>
         <a
           className="App-link"
